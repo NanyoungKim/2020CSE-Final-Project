11e442bce14544634d0d880596acc2628ef65f21
kimhyeyun <cqqwer@gmail.com>
2020-09-27 15:47:16 +0900

    Merge branch 'master' of C:\2020CSE-Final-Project with conflicts.

--- app/src/main/java/com/example/a2020cse_final_project/MainActivity.java
+++ app/src/main/java/com/example/a2020cse_final_project/MainActivity.java
@@ -10,8 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("hi");
-        System.out.println("youngju");
+        System.out.println("try_youngju");
+
     }
 
 }
+
