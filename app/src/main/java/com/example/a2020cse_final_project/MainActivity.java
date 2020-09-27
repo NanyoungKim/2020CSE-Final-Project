d4fbf4ed0c3b32023c064d47981e7d16470fa343
YOUNGJU <clleo97@naver.com>
2020-09-27 15:44:57 +0900

    tlqkf

--- app/src/main/java/com/example/a2020cse_final_project/MainActivity.java
+++ app/src/main/java/com/example/a2020cse_final_project/MainActivity.java
@@ -10,7 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("try_youngju");
+        System.out.println("hi");
+        System.out.println("youngju");
     }
 
 }
