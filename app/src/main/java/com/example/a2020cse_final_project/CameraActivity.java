72caf3d1ebc6d5becbbd19442ccabb0218904a5c
kimhyeyun <cqqwer@gmail.com>
2020-09-27 18:16:37 +0900

    PERMISSION - CAMERA v1.1

--- app/src/main/java/com/example/a2020cse_final_project/CameraActivity.java
+++ app/src/main/java/com/example/a2020cse_final_project/CameraActivity.java
@@ -1,1 +1,54 @@
-\
+package com.example.a2020cse_final_project;
+
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.TextureView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
+
+public class CameraActivity extends AppCompatActivity {
+    private TextureView textureView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        textureView = (TextureView)findViewById(R.id.textureview);
+        assert textureView != null;
+        textureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
+                openCamera();
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) {
+
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void openCamera() {
+        CameraManager manager = (CameraManager)getSystemService(Context.CAMERA_SERVICE);
+    }
+}
